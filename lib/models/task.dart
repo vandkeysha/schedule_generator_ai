77b192b2dfbbf75c28ad88dcd50578668ca06968
Echa Imuts <vanda@vandas-MacBook-Pro.local>
2026-02-17 08:53:27 +0700

    creating a model

---
+++ lib/models/task.dart
@@ -0,0 +1,14 @@
+class Task {
+  final String name;
+  final String priority;
+  final int duration;
+  final String deadline;
+
+  Task({required this.name, required this.priority, required this.duration, required this.deadline});
+
+  @override
+  String toString() {
+    return 'Task{name: $name, priority: $priority, duration: $duration, deadline: $deadline}';
+  }
+
+}
